aa7d60afaec9f37abef45aa989f8e6d327013b01
Paweł Stiasny <pawelstiasny@gmail.com>
2014-03-23 16:23:49 +0100

    run only the nondotfile task by default

--- bigos/__main__.py
+++ bigos/__main__.py
@@ -3,18 +3,10 @@
 
 from . import main, on
 
-@on(r'', dirs=None)
-def default_task(ev):
-    print 'event:', ev.path, ev.type, 'dir' if ev.is_dir else 'file'
-
 @on(r'^[^/]*/([^.][^/]*\/)*[^.][^/]*$')
 def file_task(ev):
     print 'file event:', ev.path, ev.type
     if hasattr(ev, 'flags'):
         print 'inotify flags:', ev.flags
-
-@on(r'', dirs=True)
-def dir_task(ev):
-    print 'dir event:', ev.path
 
 main('.')
